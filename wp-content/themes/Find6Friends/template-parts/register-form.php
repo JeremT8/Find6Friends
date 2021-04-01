bf0a0e6f3055032e5983f0567484dee18fda5a0b
JeremT8 <jeremytilletpro@gmail.com>
2021-04-01 13:14:31 +0200

    static integer + template-parts creates

--- wp-content/themes/Find6Friends/template-parts/register-form.php
+++ wp-content/themes/Find6Friends/template-parts/register-form.php
@@ -2,9 +2,9 @@
 <section class="hero" id="hero">
 		<div class="container">
 			<h2 class="sub-headline sub-headline-hero">
-				<span class="first-letter">f</span>il d'actualités
+				<span class="first-letter">R</span>ejoindres
 			</h2>
-			<h1 class="headline">de Rainbow six siege</h1>
+			<h1 class="headline">Find 6 Friends</h1>
 			<div class="headline-description">
 				<div class="separator">
 					<div class="line line-left"></div>
@@ -12,12 +12,13 @@
 					<div class="line line-right"></div>
 				</div>
 				<div class="single-animation">
-					<h5>Parés pour rejoindres l'aventure</h5>
-					<a href="#" class="btn cta-btn">Cliques ici</a>
+					<h5>Vous possédez deja un compte ?</h5>
+					<a href="#" class="btn cta-btn">Connectez-vous !</a>
 				</div>
 			</div>
 		</div>
 	</section>
+
 
 
 <?php get_footer() ?>
