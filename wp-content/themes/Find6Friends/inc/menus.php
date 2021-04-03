7acdb8125acfa8b8be1d5131d7b26a15cae61275
JeremT8 <jeremytilletpro@gmail.com>
2021-04-03 16:48:55 +0200

    add welcome {usernam} at top nav if user is loggedin

--- wp-content/themes/Find6Friends/inc/menus.php
+++ wp-content/themes/Find6Friends/inc/menus.php
@@ -8,3 +8,17 @@ add_action('after_setup_theme', function () {
     );
 });
 
+add_filter( 'wp_nav_menu_items', 'my_custom_menu_item');
+function my_custom_menu_item($items)
+{
+    if(is_user_logged_in())
+    {
+        $user=wp_get_current_user();
+        $name=$user->display_name; // or user_login , user_firstname, user_lastname
+        $items .= '<li style="text-transform:uppercase;text-decoration: none;
+        color: whitesmoke;text-transform: uppercase;
+        font-size: 1.6rem;
+        letter-spacing: 2px;">Bienvenue '.$name.'</a></li>';
+    }
+    return $items;
+}
